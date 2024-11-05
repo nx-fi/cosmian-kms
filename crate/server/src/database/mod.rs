e9774195c954fa1061855964676339aea2223c89
Bruno Grieder <bruno.grieder@cosmian.com>
2024-11-05 15:23:51 +0100

    module doc

--- crate/server/src/database/mod.rs
+++ crate/server/src/database/mod.rs
@@ -1,38 +1,39 @@
-/// This module contains the database implementation for the KMS server.
-/// It provides functionality for interacting with different types of databases,
-/// such as `SQLite``MySQL``PostgreSQL`eSQL, and Redis.
-///
-/// The module includes the following sub-modules:
-/// - `cached_sqlcipher`: Contains the implementation for caching SQL queries using `SQLCipher`.
-/// - `cached_sqlite_struct`: Contains the implementation for caching `SQLite` structures.
-/// - `database_trait`: Contains the trait definition for a generic database.
-/// - `mysql`: Contains the implementation for `MySQL` database.
-/// - `object_with_metadata`: Contains the implementation for objects with metadata.
-/// - `pgsql`: Contains the implementation for `PostgreSQL` database.
-/// - `redis`: Contains the implementation for Redis database.
-/// - `sqlite`: Contains the implementation for `SQLite` database.
-/// - `locate_query`: Contains utility functions for locating queries.
-/// - `migrate`: Contains functions for database migration.
-/// - `retrieve_object_utils`: Contains utility functions for retrieving objects.
-///
-/// The module also defines the following types and constants:
-/// - `KMSServer`: A type alias for the KMS server.
-/// - `DBObject`: A struct representing a database object.
-/// - `KMS_VERSION_BEFORE_MIGRATION_SUPPORT`: A constant representing the KMS version before migration support.
-/// - `PGSQL_FILE_QUERIES`: A constant representing the `PostgreSQL` file queries.
-/// - `MYSQL_FILE_QUERIES`: A constant representing the `MySQL` file queries.
-/// - `SQLITE_FILE_QUERIES`: A constant representing the `SQLite` file queries.
-///
-/// The module also includes the following functions:
-/// - `state_from_string`: Converts a string to a `StateEnumeration` value.
-///
-/// Finally, the module includes a test module for unit testing.
-///
-/// # Errors
-///
-/// This module does not define any specific errors. However, it may return errors
-/// from the underlying database operations or from the functions defined in the sub-modules.
-/// The specific error types and conditions are documented in the respective functions.
+//! This module contains the database implementation for the KMS server.
+//! It provides functionality for interacting with different types of databases,
+//! such as `SQLite`, `MySQL`, `PostgreSQL`, and `Redis`.
+//!
+//! The module includes the following submodules:
+//! - `cached_sqlcipher`: Contains the implementation for caching SQL queries using `SQLCipher`.
+//! - `cached_sqlite_struct`: Contains the implementation for caching `SQLite` structures.
+//! - `database_trait`: Contains the trait definition for a generic database.
+//! - `mysql`: Contains the implementation for `MySQL` database.
+//! - `object_with_metadata`: Contains the implementation for objects with metadata.
+//! - `pgsql`: Contains the implementation for `PostgreSQL` database.
+//! - `redis`: Contains the implementation for Redis database.
+//! - `sqlite`: Contains the implementation for `SQLite` database.
+//! - `locate_query`: Contains utility functions for locating queries.
+//! - `migrate`: Contains functions for database migration.
+//! - `retrieve_object_utils`: Contains utility functions for retrieving objects.
+//!
+//! The module also defines the following types and constants:
+//! - `KMSServer`: A type alias for the KMS server.
+//! - `DBObject`: A struct representing a database object.
+//! - `KMS_VERSION_BEFORE_MIGRATION_SUPPORT`: A constant representing the KMS version before migration support.
+//! - `PGSQL_FILE_QUERIES`: A constant representing the `PostgreSQL` file queries.
+//! - `MYSQL_FILE_QUERIES`: A constant representing the `MySQL` file queries.
+//! - `SQLITE_FILE_QUERIES`: A constant representing the `SQLite` file queries.
+//!
+//! The module also includes the following functions:
+//! - `state_from_string`: Converts a string to a `StateEnumeration` value.
+//!
+//! Finally, the module includes a test module for unit testing.
+//!
+//! # Errors
+//!
+//! This module does not define any specific errors. However, it may return errors
+//! from the underlying database operations or from the functions defined in the submodules.
+//! The specific error types and conditions are documented in the respective functions.
+
 use cosmian_kmip::kmip::{
     kmip_objects::{Object, ObjectType},
     kmip_types::StateEnumeration,
@@ -75,10 +76,10 @@ lazy_static! {
 
 /// The Database implemented using `SQLite`
 ///
-/// This class uses a connection should be cloned on each server thread
+/// This class uses a connection that should be cloned on each server thread
 #[derive(Clone)]
 /// When using JSON serialization, the Object is untagged
-/// and looses its type information, so we have to keep
+/// and loses its type information, so we have to keep
 /// the `ObjectType`. See `Object` and `post_fix()` for details
 #[derive(Serialize, Deserialize)]
 pub(crate) struct DBObject {
