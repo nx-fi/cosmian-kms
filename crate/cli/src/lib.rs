2745ddc50114f4645651f89327599e00e800e1a3
Manuthor <manu.coste@gmail.com>
2024-11-07 08:22:38 +0100

    refactor: reexpose clap actions for other CLIs (cosmian, ckms_gui)

--- crate/cli/src/lib.rs
+++ crate/cli/src/lib.rs
@@ -4,7 +4,7 @@
     future_incompatible,
     keyword_idents,
     let_underscore,
-    rust_2024_compatibility,
+    // rust_2024_compatibility,
     unreachable_pub,
     unused,
     clippy::all,
@@ -29,7 +29,10 @@
     clippy::redundant_pub_crate
 )]
 pub mod actions;
+pub mod commands;
 pub mod error;
+
+pub use commands::{ckms_main, Cli, CliCommands};
 
 #[cfg(test)]
 mod tests;
